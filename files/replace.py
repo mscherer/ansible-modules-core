ansible/ansible

commit 5728ef89f0764be9066fc1bf0fbbf7785e60f4cb

Implemented file content diff for replace module (#4479)

files/replace.py
@@ -142,15 +142,25 @@ def main():
         contents = f.read()
         f.close()
 
+    if module._diff:
+        diff = {
+            'before_header': dest,
+            'before': contents,
+        }
+
     mre = re.compile(params['regexp'], re.MULTILINE)
     result = re.subn(mre, params['replace'], contents, 0)
 
     if result[1] > 0 and contents != result[0]:
         msg = '%s replacements made' % result[1]
         changed = True
+        if module._diff:
+            diff['after_header'] = dest
+            diff['after'] = result[0]
     else:
         msg = ''
         changed = False
+        diff = dict()
 
     if changed and not module.check_mode:
         if params['backup'] and os.path.exists(dest):
@@ -160,7 +170,7 @@ def main():
         write_changes(module, result[0], dest)
 
     msg, changed = check_file_attrs(module, changed, msg)
-    module.exit_json(changed=changed, msg=msg)
+    module.exit_json(changed=changed, msg=msg, diff=diff)
 
 # this is magic, see lib/ansible/module_common.py
 from ansible.module_utils.basic import *

mscherer/ansible-modules-core

commit 705be5b7fff867c150e595480281e2c7b8bad13e

Fix replace module, broken after #4479

files/replace.py
@@ -170,7 +170,13 @@ def main():
         write_changes(module, result[0], dest)
 
     msg, changed = check_file_attrs(module, changed, msg)
-    module.exit_json(changed=changed, msg=msg, diff=diff)
+
+    result = {'changed': changed, 
+              'msg': msg}
+    if module._diff:
+        result['diff'] = diff
+
+    module.exit_json(**result)
 
 # this is magic, see lib/ansible/module_common.py
 from ansible.module_utils.basic import *